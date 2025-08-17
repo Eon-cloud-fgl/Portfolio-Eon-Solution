059a2b33834c2ef643422c8aee5e5b870487fd76
Gabriel Voos <gabrielvoos7@gmail.com>
2025-08-16 23:06:17 -0300

    Feat: inicializacion de la creacion del componente Home. Creacion del banner con animacion de fondo, titulo y texto. Falta terminar logica del useRef y useEffect.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,42 @@
 import '../styles/home.css';
+import React, { useRef, useEffect } from 'react';
+
 
 export default function Home(){
     return (
         <>
-        <h1>Home</h1>
-        <h1>Portfolio</h1>
+            <div>
+                <Banner />
+            </div>
         </>
     )
 }
+
+function Banner(){
+    const videoRef = useRef(null);
+    useEffect(() => {
+        const video = videoRef.current;
+        if (video) {
+            const timer = setTimeout(() => {
+                
+            })
+        }
+    }
+
+
+    return (
+        <div className="banner">
+            <div className="banner-content">
+                <h1>Te damos la bienvenida a Eon Solutions</h1>
+                <p>Inverti en soluciones, nosotros te las proponemos</p>
+            </div>
+
+            <div className="banner-video-container">
+                <video className='banner-video' autoPlay loop muted>
+                    <source src="/animacion_inicial.mp4" type="video/mp4" />
+                </video>
+            </div>
+        </div>
+    )
+}
+
