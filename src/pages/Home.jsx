af59aaed6ad744a4e27feb51ba38dfe4e8690532
Prominence673 <lautarosouza58@gmail.com>
2025-08-18 17:28:42 -0300

    feat : nada

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,10 @@
 import '../styles/home.css';
 
-function MainVideo(){
-    return (
-        <div className="main-image">
-            <video autoPlay loop muted playsInline>
-                <source src="5921369-hd_1920_1080_30fps.mp4" type="video/mp4" />
-                Your browser does not support the video tag.
-            </video>
-        </div>
-    );
-}
-
-
 export default function Home(){
     return (
         <>
-            <MainVideo/>
+        <h1>Home</h1>
+        <h1>Portfolio</h1>
         </>
     )
 }
