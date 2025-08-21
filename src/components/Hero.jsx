498841d9fd405b2a529d049ead41fd8d5e46bbe8
Ferchoch0 <delvalle.fernando.d@gmail.com>
2025-08-21 01:47:39 -0300

    "feat: cambios el home: -Leves cambios en el titulo principal -cambio de logo_blanco -Navbar fijo -Componente de Lenguajes"

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,14 +1,36 @@
 import { motion } from "framer-motion";
 import '../styles/hero.css';
 import { useRef, useEffect } from 'react';
+import { FaArrowRightLong } from "react-icons/fa6";
+
+function Mouse() {
+  return (
+    <div className="scroll-indicator">
+      <div className="mouse">
+        <div className="wheel"></div>
+      </div>
+      <div className="line"></div>
+      <div className="arrow"></div>
+    </div>
+  );
+}
 
 export default function Hero() {
   return (
     <section className="hero">
       {/* Contenido principal */}
       <div className="hero-content">
-        <motion.h1
+        <motion.h2
           initial={{ opacity: 0, y: -30 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8 }}
+          className="hero-pretitle"
+        >
+          Somos
+        </motion.h2>
+
+        <motion.h1
+          initial={{ opacity: 0, y: -60 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 1 }}
           className="glitch"
@@ -23,7 +45,7 @@ export default function Hero() {
           transition={{ duration: 1.2 }}
           className="text-hero"
         >
-          Innovación y estrategia para tu negocio
+          Combinamos innovación y estrategia para transformar tu negocio y potenciar su crecimiento
         </motion.p>
 
         <motion.button
@@ -31,17 +53,21 @@ export default function Hero() {
           whileTap={{ scale: 0.95 }}
           className="cta-button"
         >
-          Work with Us
+          <FaArrowRightLong />
         </motion.button>
       </div>
 
       {/* Esfera decorativa */}
       <motion.div
         className="hero-sphere"
-        initial={{ opacity: 0, x: 30 }}
+        initial={{ opacity: 0, x: 120 }}
         animate={{ opacity: 1, x: 0 }}
-        transition={{ duration: 1 }}
+        transition={{ duration: 2 }}
       />
+
+      <article className="mouse-container">
+        <Mouse />
+      </article>
     </section>
   );
 }
